bd686794ecb99dfd96ae210b71ce4c5b5b6da1f8
bihanikeshav <60587842+bihanikeshav@users.noreply.github.com>
2022-10-02 06:49:00 +0530

    Changes to be committed: 	modified:   index.html 	modified:   index.js 	modified:   style.css

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 (function() {
   'use strict';
   window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
     var validation = Array.prototype.filter.call(forms, function(form) {
       form.addEventListener('submit', function(event) {
         if (form.checkValidity() === false) {
           event.preventDefault();
           event.stopPropagation();
         }
-
         form.classList.add('was-validated');
       }, false);
     });
   }, false);
 })();
+
+
+var cnfPass=document.getElementById("password2");
+cnfPass.onI = function(){
+    var passValue = document.getElementById("password")
+    var confpassValue = document.getElementById("password2")
+    if(passValue != confpassValue) {
+        document.getElementsById("password2").setClass("is-invalid")
+     }
+     else{
+        document.getElementsById("password2").setClass("is-valid")
+     }
+}
